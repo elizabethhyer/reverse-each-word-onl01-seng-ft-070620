00fbc0a72314e31cb957b7e6e2c5db39f61d439b
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-08 19:28:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(phrase)
-  #reverse_it = []
+  reverse_it = []
   reverse_word = phrase.split(" ") 
   reverse_word.collect do |sentence1|
-  #reverse_it << 
-  sentence1.reverse
+  reverse_it << sentence1.reverse
   end 
   reverse_word.join(" ")
   #reverse_it.join(" ")
